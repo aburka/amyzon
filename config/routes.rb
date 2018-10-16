1d003681fe132c6284fdfca9ae9c2336f5e48cb4
aburka <aburka@gmail.com>
2018-10-16 12:18:02 -0400

    add sales routes and controller, some views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "books#index"
-  resources :books, only: :show
+  resources :books, only: :show do
+    resources :sales, only: [:new, :create]
+  end
+
+  get "/sales", to: "sales#index"
 
   get "styleguide", to: "styleguide#index"
 end
